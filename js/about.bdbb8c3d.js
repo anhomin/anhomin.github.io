c19865c9cbe91be7ca33c2cdc43feff1a3fb016a
anhomin <108154243+anhomin@users.noreply.github.com>
2022-07-01 11:18:25 +0900

    new version

---
+++ js/about.bdbb8c3d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkrelay"]=self["webpackChunkrelay"]||[]).push([[443],{4335:function(n,a,e){e.r(a),e.d(a,{default:function(){return f}});var r=e(3396);const u={class:"about"},t=(0,r._)("h1",null,"This is an about page",-1),s=[t];function c(n,a){return(0,r.wg)(),(0,r.iD)("div",u,s)}var i=e(89);const l={},o=(0,i.Z)(l,[["render",c]]);var f=o}}]);
+//# sourceMappingURL=about.bdbb8c3d.js.map
